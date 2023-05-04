5ee36cb20d68f7508dea067ce09290d03fb7fa88
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-05-04 21:18:15 +0100

    Clear all and check function

---
+++ src/module/interaction.js
@@ -0,0 +1,19 @@
+const restructureData = (datas) => datas.map((el, index) => {
+  el.index = index + 1;
+  return el;
+});
+
+export const check = (index) => {
+  const storedData = JSON.parse(localStorage.getItem('todos'));
+  const updatedData = storedData.map((data) => {
+    if (data.index === index) data.complete = !data.complete;
+    return data;
+  });
+  localStorage.setItem('todos', JSON.stringify(updatedData));
+};
+
+export const clearAllComplete = () => {
+  const storedData = JSON.parse(localStorage.getItem('todos'));
+  const updatedData = storedData.filter((data) => !data.complete);
+  localStorage.setItem('todos', JSON.stringify(restructureData(updatedData)));
+};
